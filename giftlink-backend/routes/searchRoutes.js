5f85e60b2426a6bcea731ebd28b6673cdc251053
gatogato999 <ggaattoo38@gmail.com>
2025-12-24 18:26:12 -0500

    perfecting the search route

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -21,20 +21,20 @@ router.get('/', async (req, res, next) => {
         // Task 3: Add other filters to the query
         if (req.query.category) {
             query.category = req.query.category;
-query.condition = req.query.condition;
-query.age_years = { $lte: parseInt(req.query.age_years) };
+
+
         }
-        if (req.query.condition) {
-            await collection.find(query).toArray()
+        if (req.query.condition) {query.condition = req.query.condition;
+            
         }
-        if (req.query.age_years) {
-            // {{insert code here}}
+        if (req.query.age_years) {   
+              // {{insert code here}}
             query.age_years = { $lte: parseInt(req.query.age_years) };
         }
 
         // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
         // {{insert code here here}}
-
+        const gifts=  await collection.find(query).toArray()
         res.json(gifts);
     } catch (e) {
         next(e);
